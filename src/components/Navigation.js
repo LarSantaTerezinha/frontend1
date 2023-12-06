01d655a29d5746742c0af4288c8ff0efbdcb7c04
LarSantaTerezinha <144362922+LarSantaTerezinha@users.noreply.github.com>
2023-12-06 20:19:15 -0300

    mais outro

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,6 +2,16 @@ import React from 'react';
 import {Navbar,Nav} from 'react-bootstrap';
 import logo from '../static/logo.png'
 import "../App.css";
+import {NavLink} from 'react-router-dom';
+
+import {
+  CDBSidebar,
+  CDBSidebarContent,
+  CDBSidebarFooter,
+  CDBSidebarHeader,
+  CDBSidebarMenu,
+  CDBSidebarMenuItem,
+} from 'cdbreact';
 
 
 const Navigation = () => {
@@ -16,9 +26,29 @@ const Navigation = () => {
               className="d-inline-block align-center"
               alt="React Bootstrap logo"
             />{' '}
-            Student Management System
+            Sistema de Gerenciamento de Estudantes
         </Navbar.Brand>
     </Navbar>
+    <div className='sidebar'>
+    <CDBSidebar textColor="#333" backgroundColor="#f0f0f0">
+    <CDBSidebarHeader prefix={<i className="fa fa-bars" />}>
+      Navigation
+    </CDBSidebarHeader>
+    <CDBSidebarContent>
+      <CDBSidebarMenu>
+        <NavLink to="/">
+          <CDBSidebarMenuItem icon="home">Home</CDBSidebarMenuItem>
+        </NavLink>
+        <NavLink to="/student">
+          <CDBSidebarMenuItem icon="list">Lista de Estudantes</CDBSidebarMenuItem>
+        </NavLink>
+        <NavLink to="/manage">
+          <CDBSidebarMenuItem icon="user">Gerenciar Estudantes</CDBSidebarMenuItem>
+            </NavLink>
+          </CDBSidebarMenu>
+        </CDBSidebarContent>
+        </CDBSidebar>
+    </div>
     </div>
   );
 };
